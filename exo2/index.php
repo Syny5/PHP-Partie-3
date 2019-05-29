a4e4845e9aee6aa3d54a4e8ff9a9fc20ab365ea9
syn <vicdemilly@gmail.com>
2019-05-29 11:11:00 +0200

    initial commit

---
+++ exo2/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <title>Exercice 2</title>
+  </head>
+  <body>
+      <!-- container Bootstrap -->
+      <div class="container-fluid">
+          <!-- header -->
+          <header>
+            <div class="jumbotron jumbotron-fluid">
+              <div class="container">
+                <h1 class="display-3">Partie 3 - Exercice 2</h1>
+                <p class="lead">Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+                Tant que la première variable n'est pas supérieure à 20 :
+
+                - multiplier la première variable avec la deuxième;
+                - afficher le résultat;
+                - incrementer la première variable.</p>
+              </div>
+            </div>
+          </header>
+          <!-- main -->
+          <main>
+            <?php
+            $secondNumber = 65;
+            for ($firstNumber = 0 ; $firstNumber < 20 ; $firstNumber++){
+              echo '<p>' . $firstNumber * $secondNumber . '</p>';
+            }
+            ?>
+          </main>
+          <!-- footer -->
+          <footer>
+
+          </footer>
+      </div>
+      <!-- scripts JQuery, Popper, Angular et Bootstrap-->
+      <script src="assets/js/jquery-3.4.0.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <!-- mon script principal -->
+      <script src="assets/js/main.js"></script>
+  </body>
+</html>
---
+++ exo6/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <title>Exercice 6</title>
+  </head>
+  <body>
+      <!-- container Bootstrap -->
+      <div class="container-fluid">
+          <!-- header -->
+          <header>
+            <div class="jumbotron jumbotron-fluid">
+              <div class="container">
+                <h1 class="display-3">Partie 3 - Exercice 6</h1>
+                <p class="lead">En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.</p>
+              </div>
+            </div>
+          </header>
+          <!-- main -->
+          <main>
+            <?php
+            for ($i = 20; $i >= 0; $i--){
+              echo '<p>' . $i . '. C\'est presque bon.</p>';
+            }
+            ?>
+          </main>
+          <!-- footer -->
+          <footer>
+
+          </footer>
+      </div>
+      <!-- scripts JQuery, Popper, Angular et Bootstrap-->
+      <script src="assets/js/jquery-3.4.0.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <!-- mon script principal -->
+      <script src="assets/js/main.js"></script>
+  </body>
+</html>
